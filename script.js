335bab13fe8580c1c1fcc1458a770036e788aa67
Kleber Henrique <yuk9.gabriel@gmail.com>
2023-03-14 18:09:01 -0300

    update

--- script.js
+++ script.js
@@ -1,89 +1,92 @@
 const options = {
-    series: [
-      {
-        name: "cambio",
-        data: [
-          {
-            x: new Date("2018-02-12").getTime(),
-            y: 5.18,
-          },
-          {
-            x: new Date("2018-02-13").getTime(),
-            y: 5.3,
-          },
-          {
-            x: new Date("2018-02-14").getTime(),
-            y: 5.18,
-          },
-          {
-            x: new Date("2018-02-15").getTime(),
-            y: 5.11,
-          },
-          {
-            x: new Date("2018-02-16").getTime(),
-            y: 5.18,
-          },
-          {
-            x: new Date("2018-02-17").getTime(),
-            y: 5.25,
-          },
-          {
-            x: new Date("2018-02-18").getTime(),
-            y: 5.18,
-          },
-          {
-            x: new Date("2018-02-19").getTime(),
-            y: 5.2,
-          },
-        ],
-      },
-    ],
-    chart: {
-      height: 350,
-      type: "area",
-      toolbar: {
-        show: false,
+  series: [
+    {
+      name: "cambio",
+      data: [
+        {
+          x: new Date("2018-02-12").getTime(),
+          y: 5.18,
+        },
+        {
+          x: new Date("2018-02-13").getTime(),
+          y: 5.3,
+        },
+        {
+          x: new Date("2018-02-14").getTime(),
+          y: 5.18,
+        },
+        {
+          x: new Date("2018-02-15").getTime(),
+          y: 5.11,
+        },
+        {
+          x: new Date("2018-02-16").getTime(),
+          y: 5.18,
+        },
+        {
+          x: new Date("2018-02-17").getTime(),
+          y: 5.25,
+        },
+        {
+          x: new Date("2018-02-18").getTime(),
+          y: 5.18,
+        },
+        {
+          x: new Date("2018-02-19").getTime(),
+          y: 5.2,
+        },
+      ],
+    },
+  ],
+  chart: {
+    height: 350,
+    type: "area",
+    toolbar: {
+      show: false,
+    },
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  stroke: {
+    curve: "straight",
+  },
+  yaxis: {
+    min: 5,
+    tickAmount: 4,
+    labels: {
+      formatter: (value) => {
+        return value.toFixed(1).replace(".", ",");
       },
     },
-    dataLabels: {
+  },
+  xaxis: {
+    labels: {
+      show: false,
+    },
+    tooltip: {
       enabled: false,
     },
-    stroke: {
-      curve: "straight",
+    axisTicks: {
+      show: false,
     },
-    yaxis: {
-      min: 5,
-      tickAmount: 4,
-      labels: {
-        formatter: (value) => {
-            return value.toFixed(1).replace(".",",")
-        }
-      },
+  },
+  fill: {
+    gradient: {
+      shadeIntensity: 1,
+      opacityFrom: 0.7,
+      opacityTo: 0.9,
+      stops: [0, 90, 100],
     },
-    xaxis: {
-      labels: {
-        show: false,
-      },
-      tooltip: {
-        enabled: false,
-      },
-      axisTicks: {
-        show: false,
-      },
-    },
-    fill: {
-      gradient: {
-        shadeIntensity: 1,
-        opacityFrom: 0.7,
-        opacityTo: 0.9,
-        stops: [0, 90, 100],
-      },
-    },
-    colors: ["#7C3AED"],
-    tooltip: {
-      custom: function ({ series, seriesIndex, dataPointIndex, w }) {
-        return `<div class="tooltip">
-      <span>${String(series[seriesIndex][dataPointIndex]).replace('.', ',')}</span>
+  },
+  colors: ["#7C3AED"],
+  tooltip: {
+    custom: function ({ series, seriesIndex, dataPointIndex, w }) {
+      return `<div class="tooltip">
+      <span>${String(series[seriesIndex][dataPointIndex]).replace(
+        ".",
+        ","
+      )}</span>
       <span>${new Date(
         w.globals.seriesX[seriesIndex][dataPointIndex]
       ).toLocaleDateString("pt-BR", {
@@ -91,10 +94,46 @@ const options = {
         month: "short",
         day: "numeric",
       })}</span>
-      </div>`
-      },
+      </div>`;
     },
-  }
-  
-  const chart = new ApexCharts(document.querySelector("#chart"), options)
-  chart.render()
+  },
+};
+
+const chart = new ApexCharts(document.querySelector("#chart"), options);
+chart.render();
+
+const optionMenu = document.querySelector(".dropdown"),
+  selectOption = optionMenu.querySelector(".selected"),
+  optionsSelect = optionMenu.querySelectorAll(".option");
+
+optionsSelect.forEach((option) => {
+  option.addEventListener("click", () => {
+    selectOption.innerHTML = option.innerHTML;
+  });
+});
+
+const inputCurrency = document.querySelector(".amount");
+
+// function numberFormatter() {
+//   const numberFormatted = new Intl.NumberFormat("en-US", {
+//     style: "currency",
+//     currency: "USD",
+//   }).format(inputCurrency.value);
+
+//   return numberFormatted;
+// }
+
+// const numberFormatted = new Intl.NumberFormat("en-US", {
+//     style: "currency",
+//     currency: "USD",
+//   });
+
+inputCurrency.addEventListener("input", (event) => {
+  let amount = event.data;
+  let formatted = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(amount);
+
+  return (inputCurrency.value = formatted);
+});
